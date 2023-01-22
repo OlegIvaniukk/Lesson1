608dc9e5c2f05abe4029094fbe2197505614497e
Olegivaniukk <olegivaniuk@gmail.com>
2023-01-22 17:28:15 +0200

    HW1

--- src/main/java/org/example/myClass.java
+++ src/main/java/org/example/myClass.java
@@ -1,4 +1,4 @@
- class myClass {
+class myClass {
     int Year = 2023;
     public static int myMethod(int a, int b) {
         int c = a + b;
@@ -7,4 +7,4 @@
     public static void main(String[] args) {
         myMethod(1, 2);
     }
- }
+}
